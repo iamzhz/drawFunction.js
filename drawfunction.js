4e72cb03214439cbfaa696a21fe57b9224743368
iamzhz <zhzqqyd@qq.com>
2024-02-11 21:35:03 +0800

    add

--- drawfunction.js
+++ drawfunction.js
@@ -1,12 +1,14 @@
 class DrawFunc {
-    constructor(canvas, width, height) {
+    constructor(canvas, width, height, continuityDegree = 0.1) {
         this.context = canvas.getContext('2d');
         this.imageData = this.context.createImageData(width, height);
         this.width = width;
         this.height = height;
+        this.continuity = continuityDegree;
     }
     drawPoint(x, y, color=[0, 0, 0, 255]) {
-        y = this.height - 1 - y;
+        x = Math.round(x);
+        y = Math.round(this.height - 1 - y);
         const index = (x + y * this.width) * 4;
         this.imageData.data[index] = color[0]; // R
         this.imageData.data[index + 1] = color[1]; // G
@@ -19,37 +21,35 @@ class DrawFunc {
         }
         this.updateCanvas();
     }
-    /*connectPoints(p1, p2, color=[0, 0, 0, 255]) {
-        let x0 = Math.floor(p1.x);
-        let y0 = Math.floor(p1.y);
-        let x1 = Math.floor(p2.x);
-        let y1 = Math.floor(p2.y);
-        let dx = Math.abs(x1 - x0);
-        let dy = -Math.abs(y1 - y0);
-        let sx = (x0 < x1) ? 1 : -1;
-        let sy = (y0 < y1) ? 1 : -1;
-        let err = ((dx > dy) ? dx : -dy) / 2;
-        while (true) {
-            this.drawPoint(x0, y0, color);
-            if (x0 === x1 && y0 === y1) break;
-    
-            let e2 = err;
-            if (e2 > -dx) {
-                err -= dy;
-                x0 += sx;
-            }
-            if (e2 < 0) {
-                err += dx;
-                y0 += sy;
-            }
+    getDistance(p1, p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+    getRatio(p1, p2) { // ratio -> x / y
+        let x = p1.x - p2.x;
+        let y = p1.y - p2.y;
+        return (x / y);
+    }
+    connectPoints(p1, p2, color=[0, 0, 0, 255]) {
+        // Make p1 always to the left of p2
+        if (p2.x < p1.x) {
+            [p1, p2] = [p2, p1];
         }
-        this.updateCanvas();
+        let points = [];
+        let distance = this.getDistance(p1, p2);
+        let stepX = (p2.x - p1.x) / distance;
+        let stepY = (p2.y - p1.y) / distance;
+        for (let i = this.continuity; i < distance; i += this.continuity) {
+            let x = p1.x + i * stepX;
+            let y = p1.y + i * stepY;
+            points.push({'x': x, 'y': y});
+        }
+        this.drawPoints(points, color);
     }
     connectAllPoints(points, color=[0, 0, 0, 255]) {
-        for (let i = 0; i < points.length - 1; i++) {
+        for(let i = 0; i < points.length - 1; i++) {
             this.connectPoints(points[i], points[i + 1], color);
         }
-    }*/
+    }
     drawFunc(func, color=[0, 0, 0, 255]) {
         let points = [];
         for (let x = 0; x < this.width; x++) {
@@ -59,7 +59,7 @@ class DrawFunc {
         }
         //console.table(points);
         this.drawPoints(points, color);
-        //this.connectAllPoints(points, color);
+        this.connectAllPoints(points, color);
     }
     updateCanvas() {
         this.context.putImageData(this.imageData, 0, 0);
